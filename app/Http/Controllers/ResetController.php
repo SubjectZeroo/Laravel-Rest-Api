b8628ec4972c9287dccffd1cdac1d70b6c34893a
Jafeht Soturno <jasordev@gmail.com>
2021-03-08 16:42:41 -0400

    update readme

--- app/Http/Controllers/ResetController.php
+++ app/Http/Controllers/ResetController.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Facades\Artisan;
 
 class ResetController extends Controller
 {
+
     public function reset()
     {
         Artisan::call('migrate:fresh');
         return 'OK'; //STATUS CODE IS 200
     }
+
 }
